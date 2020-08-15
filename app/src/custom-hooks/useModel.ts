e3d9bb69b38c14903cec5015886120cecc5a5df3
welvin21 <wewe2712@connect.hku.hk>
2020-08-15 21:03:00 +0800

    Format code

--- app/src/custom-hooks/useModel.ts
+++ app/src/custom-hooks/useModel.ts
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import * as tf from '@tensorflow/tfjs';
+import { useEffect, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
 
 export const useModel = () => {
   const modelURL = "http://localhost:8000/model.json";
@@ -8,10 +8,10 @@ export const useModel = () => {
   useEffect(() => {
     const fetchModel = async () => {
       try {
-        const model = await tf.loadLayersModel(modelURL)
+        const model = await tf.loadLayersModel(modelURL);
         setModel(model);
       } catch (error) {
-        console.log({ error })
+        console.log({ error });
       }
     };
 
