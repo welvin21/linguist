9ffe8122b5b1f973458e73ad0f8ffb108a17a422
seanchok <seanchok@gmail.com>
2020-08-16 11:21:06 +0800

    port to model in caption

--- app/src/components/Caption.tsx
+++ app/src/components/Caption.tsx
@@ -1,22 +1,28 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect } from "react";
 
 interface CaptionProps {
   start: boolean;
+  text: string;
+  setText: (text: string) => void;
 }
 
-export const Caption: FC<CaptionProps> = ({ start }) => {
+export const Caption: FC<CaptionProps> = ({ start, text, setText }) => {
   const characters = ["h", "e", "l", "l", "o", "w", "o", "r", "l", "d"];
 
-  const [text, setText] = useState<string>("");
+  // useEffect(() => {
+  //   if (start && text.length < characters.length) {
+  //     const loop = setTimeout(() => {
+  //       setText(`${text}${characters[text.length]}`);
+  //     }, 750);
+  //     return () => clearTimeout(loop);
+  //   }
+  // }, [text, start]);
 
-  useEffect(() => {
-    if (start && text.length < characters.length) {
-      const loop = setTimeout(() => {
-        setText(`${text}${characters[text.length]}`);
-      }, 750);
-      return () => clearTimeout(loop);
-    }
-  }, [text, start]);
-
-  return <div className={"text-center text-xl"}>{text}</div>;
+  return (
+    <div className={"mx-auto max-w-lg overflow-hidden"}>
+      <p className={"text-center"} style={{ direction: "rtl" }}>
+        {text}
+      </p>
+    </div>
+  );
 };
